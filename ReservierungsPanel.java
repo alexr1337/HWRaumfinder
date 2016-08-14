6bc5a8305c4ac89e9d1844f64fec1b90077f1426
bzrn <s_zerrahn@stud.hwr-berlin.de>
2016-08-14 18:27:42 +0200

    Änderungshistorie

--- ReservierungsPanel.java
+++ ReservierungsPanel.java
@@ -13,9 +13,54 @@ import java.util.Calendar;
 import java.util.Date;
 
 /**
- * <strong>Zweck:</strong> Definiert Design und Funktionalitäten des Reservierungsvorganges ink. Detailanzeige des zu reservierenden Raumes
- * <p><strong>Änderungshistorie:</strong></p>
- * @version 2.6
+ * <strong> Zweck: </strong> Definiert Design und Funktionalitaeten des Reservierungsvorganges inklusive 
+ * Detailanzeige des zu reservierenden Raumes
+ * 
+ * <h2>Aenderungshistorie:</h2>
+ * <ol>
+ * 	<li>
+ * 		<ul>
+ * 			<li> <strong> Version: </strong> 1.0 </li>
+ *			<li> <strong> Datum: </strong> 06.08.16 </li>
+ *			<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 			<li> <strong> Beschreibung: </strong> Erstellung </li>
+ *		 </ul>
+ *	</li>
+ *	<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 2.0 </li>
+ *			<li> <strong> Datum: </strong> 10.08.16 </li>
+ *			<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 			<li> <strong> Beschreibung: </strong> Major Update </li>
+ *		</ul>
+ *	</li>
+ *	<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 2.1 </li>
+ *			<li> <strong> Datum: </strong> 11.08.16 </li>
+ *			<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 			<li> <strong> Beschreibung: </strong> Bug Fixes </li>
+ *		</ul>
+ *	</li>
+ *	<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 2.2 </li>
+ *			<li> <strong> Datum: </strong> 12.08.16 </li>
+ *			<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 			<li> <strong> Beschreibung: </strong> Kommentare/Unit-Spezifikationen </li>
+ *		</ul>
+ *	</li>
+ *	<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 2.3 </li>
+ *			<li> <strong> Datum: </strong> 13.08.16 </li>
+ *			<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 			<li> <strong> Beschreibung: </strong> Minor Bug Fixes </li>
+ *		</ul>
+ *	</li>
+ * </ol>
+ * 
+ * @version 2.3
  * @author Alexander Reichenbach
  *
  */
