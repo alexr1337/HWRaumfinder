fc1234c38c68418046aabf58e4dce2b894897205
bzrn <s_zerrahn@stud.hwr-berlin.de>
2016-08-14 19:11:59 +0200

    Update OnlineEinleser.java

--- OnlineEinleser.java
+++ OnlineEinleser.java
@@ -13,6 +13,75 @@ import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * <strong>Zweck:</strong> 
+ * <h2>Aenderungshistorie:</h2>
+ * 
+ * <ol>
+ * 		<li>
+ * 			<ul>
+ * 				<li> <strong> Version: </strong> 1.0 </li>
+ *				<li> <strong> Datum: </strong> 25.06.16 </li>
+ *				<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 				<li> <strong> Beschreibung: </strong> Initiale Befuellung </li>
+ *			 </ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 1.1 </li>
+ *				<li> <strong> Datum: </strong> 28.07.16 </li>
+ *				<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 				<li> <strong> Beschreibung: </strong> Package Verarbeitung hinzugefuegt </li>
+ *			</ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 2.0 </li>
+ *				<li> <strong> Datum: </strong> 06.08.16 </li>
+ *				<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 				<li> <strong> Beschreibung: </strong> Major Update </li>
+ *			</ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 3.0 </li>
+ *				<li> <strong> Datum: </strong> 10.08.16 </li>
+ *				<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 				<li> <strong> Beschreibung: </strong> Major Update </li>
+ *			</ul>
+ *		</li>
+ *  *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 3.1 </li>
+ *				<li> <strong> Datum: </strong> 11.08.16 </li>
+ *				<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 				<li> <strong> Beschreibung: </strong> Interfaces implementiert </li>
+ *			</ul>
+ *		</li>
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 3.2 </li>
+ *				<li> <strong> Datum: </strong> 12.08.16 </li>
+ *				<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 				<li> <strong> Beschreibung: </strong> Kommentare/Unit-Spezifikationen </li>
+ *			</ul>
+ *		</li>
+
+ *		<li>
+ *			<ul>
+ *				<li> <strong> Version: </strong> 3.3 </li>
+ *				<li> <strong> Datum: </strong> 13.08.16 </li>
+ *				<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 				<li> <strong> Beschreibung: </strong> Bug Fixes </li>
+ *			</ul>
+ *		</li>
+ *
+ * </ol>
+ * @version 3.3
+ * @author Alexander Reichenbach
+ *
+ */
+
 public class OnlineEinleser {
 
     private int fileCounter, errCounter;
