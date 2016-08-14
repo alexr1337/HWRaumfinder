868586e8730878ee66ba6fbfc56ce351cb46a3d7
bzrn <s_zerrahn@stud.hwr-berlin.de>
2016-08-14 18:28:54 +0200

    Änderungshistorie

--- ReservierungsverwaltungsPanel.java
+++ ReservierungsverwaltungsPanel.java
@@ -7,9 +7,36 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * <strong>Zweck:</strong> Definiert Design und Funktionalitäten der Reservierungsverwaltung
- * <p><strong>Änderungshistorie:</strong></p>
- * @version 2.4
+ * <strong> Zweck: </strong> Definiert Design und Funktionalitaeten der Reservierungsverwaltung
+ * <h2>Aenderungshistorie:</h2>
+ * <ol>
+ * 	<li>
+ * 		<ul>
+ * 			<li> <strong> Version: </strong> 1.0 </li>
+ *			<li> <strong> Datum: </strong> 06.08.16 </li>
+ *			<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 			<li> <strong> Beschreibung: </strong> Erstellung </li>
+ *		 </ul>
+ *	</li>
+ *	<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 2.0 </li>
+ *			<li> <strong> Datum: </strong> 10.08.16 </li>
+ *			<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 			<li> <strong> Beschreibung: </strong> Major Update </li>
+ *		</ul>
+ *	</li>
+ *	<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 2.1 </li>
+ *			<li> <strong> Datum: </strong> 12.08.16 </li>
+ *			<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 			<li> <strong> Beschreibung: </strong> Kommentare/Unit-Spezifikationen </li>
+ *		</ul>
+ *	</li>
+ * </ol>
+ * 
+ * @version 2.1
  * @author Alexander Reichenbach
  *
  */
