bd96feeb243033bd406900c065a4072e89c53af6
bzrn <s_zerrahn@stud.hwr-berlin.de>
2016-08-14 18:01:58 +0200

    Update Zeitraum.java

--- Zeitraum.java
+++ Zeitraum.java
@@ -7,10 +7,76 @@ import java.util.Date;
 
 
 /**
- * <strong>Zweck:</strong> Ermöglicht das Erzeugen von Zeitraum-Objekten mit einer bestimmten Dauer, Start- und Endzeitpunkt
- * <p><strong>Änderungshistorie:</strong> ... </p>
- *
- * @version 2.2
+ * <strong> Zweck: </strong> Ermoeglicht das Erzeugen von Zeitraum-Objekten mit einer bestimmten Dauer, Start- und Endzeitpunkt
+ * <h2>Aenderungshistorie:</h2> 
+ *<ol>
+ *   <li>
+ *     <ul>
+ *		<li> <strong> Version: </strong> 1.0 </li>
+ *		<li> <strong> Datum: </strong> 25.06.16 </li>
+ *		<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 		<li> <strong> Beschreibung: </strong> Initiale Befüllung </li>
+ *	</ul>
+ *   </li>
+ *   <li>
+ *	<ul>
+ * 		<li> <strong> Version: </strong> 2.0 </li>
+ *		<li> <strong> Datum: </strong> 11.07.16 </li>
+ * 		<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 		<li> <strong> Beschreibung: </strong> Major Update </li>
+ *	</ul>
+ *   </li>
+ *   <li>
+ *	<ul>
+ *		<li> <strong> Version: </strong> 2.1 </li>
+ *		<li> <strong> Datum: </strong> 28.07.16 </li>
+ * 		<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ *		<li> <strong> Beschreibung: </strong> toString() hinzugefügt </li>
+ *	</ul>
+ *   </li>
+ *   <li>
+ * 	<ul>
+ * 		<li> <strong> Version: </strong> 2.2 </li>
+ * 		<li> <strong> Datum: </strong> 02.08.16 </li>
+ * 		<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 		<li> <strong> Beschreibung: </strong> Implements Serializable </li>
+ *	</ul>
+ *   </li>
+ *   <li>
+ *	<ul>
+ * 		<li> <strong> Version: </strong> 3.0 </li>
+ *		<li> <strong> Datum: </strong> 06.08.16 </li>
+ * 		<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 		<li> <strong> Beschreibung: </strong> Major Update </li>
+ *	</ul>
+ *   </li>
+ *   <li>
+ *	<ul>
+ * 		<li> <strong> Version: </strong> 3.1 </li>
+ *		<li> <strong> Datum: </strong> 11.08.16 </li>
+ * 		<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 		<li> <strong> Beschreibung: </strong> Kommentare/Unit-Spezifikation </li>
+ *	</ul>
+ *   </li>
+ *   <li>
+ *	<ul>
+ * 		<li> <strong> Version: </strong> 3.2 </li>
+ *		<li> <strong> Datum: </strong> 11.08.16 </li>
+ * 		<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 		<li> <strong> Beschreibung: </strong> Bug Fixes </li>
+ *	</ul>
+ *   </li>
+ *   <li>
+ *	<ul>
+ * 		<li> <strong> Version: </strong> 3.3 </li>
+ *		<li> <strong> Datum: </strong> 12.08.16 </li>
+ * 		<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 		<li> <strong> Beschreibung: </strong> Bug Fixes </li>
+ *	</ul>
+ *   </li>
+ * </ol>
+ * 
+ * @version 3.3
  * @author Hanna Behnke, Alexander Reichenbach
  *
  */
