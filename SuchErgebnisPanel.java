a4509aca6e4cb77c92b73b4310da22cbb2b8e004
bzrn <s_zerrahn@stud.hwr-berlin.de>
2016-08-14 18:31:11 +0200

    Änderungshistorie

--- SuchErgebnisPanel.java
+++ SuchErgebnisPanel.java
@@ -8,8 +8,34 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * <strong>Zweck:</strong> Definiert das Design der gelieferten Ergebnisse einer Suche
- * <p><strong>Änderungshistorie:</strong></p>
+ * <strong> Zweck: </strong> Definiert das Design der gelieferten Ergebnisse einer Suche
+ * <h2>Aenderungshistorie:</h2>
+ * <ol>
+ * 	<li>
+ * 		<ul>
+ * 			<li> <strong> Version: </strong> 1.0 </li>
+ *			<li> <strong> Datum: </strong> 06.08.16 </li>
+ *			<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 			<li> <strong> Beschreibung: </strong> Erstellung </li>
+ *		 </ul>
+ *	</li>
+ *	<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 2.0 </li>
+ *			<li> <strong> Datum: </strong> 10.08.16 </li>
+ *			<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 			<li> <strong> Beschreibung: </strong> Major Update </li>
+ *		</ul>
+ *	</li>
+ *	<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 2.1 </li>
+ *			<li> <strong> Datum: </strong> 12.08.16 </li>
+ *			<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 			<li> <strong> Beschreibung: </strong> Kommentare/Unit-Spezifikationen </li>
+ *		</ul>
+ *	</li>
+ * </ol>
  * @version 2.1
  * @author Alexander Reichenbach
  *
