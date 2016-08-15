346ba006d5d75ad1fe7c6104b936b8ff9dca6985
agesb <hanna.behnke@gmail.com>
2016-08-15 21:50:54 +0200

    Update Änderungshistorie

--- SuchPanel.java
+++ SuchPanel.java
@@ -19,9 +19,44 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 
 /**
- * <strong>Zweck:</strong> Definiert Design und Funktionalitäten der Suche nach Kriterien
- * <p><strong>Änderungshistorie:</strong></p>
- * @version 2.4
+ * <strong> Zweck: </strong> Definiert Design und Funktionalitaeten der Suche nach Kriterien
+ * <h2>Aenderungshistorie:</h2>
+ * <ol>
+ * 	<li>
+ * 		<ul>
+ * 			<li> <strong> Version: </strong> 1.0 </li>
+ *			<li> <strong> Datum: </strong> 06.08.16 </li>
+ *			<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 			<li> <strong> Beschreibung: </strong> Erstellung und Design </li>
+ *		 </ul>
+ *	</li>
+ *	<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 1.1 </li>
+ *			<li> <strong> Datum: </strong> 10.08.16 </li>
+ *			<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 			<li> <strong> Beschreibung: </strong> Überarbeitung Design </li>
+ *		</ul>
+ *	</li>
+ *<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 1.2 </li>
+ *			<li> <strong> Datum: </strong> 10.08.16 </li>
+ *			<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 			<li> <strong> Beschreibung: </strong> Implementierung ActionListener </li>
+ *		</ul>
+ *	</li>
+ *	<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 1.3 </li>
+ *			<li> <strong> Datum: </strong> 12.08.16 </li>
+ *			<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 			<li> <strong> Beschreibung: </strong> Kommentare und Unit-Spezifikation </li>
+ *		</ul>
+ *	</li>
+ 
+ * </ol>
+ * @version 1.3
  * @author Alexander Reichenbach
  *
  */
