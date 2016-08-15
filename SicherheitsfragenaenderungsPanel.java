0594be53efb081b5337cadc41c8ae57c91e82eca
agesb <hanna.behnke@gmail.com>
2016-08-15 21:56:26 +0200

    Update Änderungshistorie

--- SicherheitsfragenaenderungsPanel.java
+++ SicherheitsfragenaenderungsPanel.java
@@ -7,9 +7,36 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * <strong>Zweck:</strong> Definiert Design und Funktionalitäten des Sicherheitsfragenänderungs-Panels
- * <p><strong>Änderungshistorie:</strong></p>
- * @version 2.0
+ * <strong> Zweck: </strong> Definiert Design und Funktionalitäten des Sicherheitsfragenaenderungs-Panels
+ * <h2>Aenderungshistorie:</h2>
+ * <ol>
+ * 	<li>
+ * 		<ul>
+ * 			<li> <strong> Version: </strong> 1.0 </li>
+ *			<li> <strong> Datum: </strong> 06.08.16 </li>
+ *			<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 			<li> <strong> Beschreibung: </strong> Erstellung und Design</li>
+ *		 </ul>
+ *	</li>
+ *	<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 1.1 </li>
+ *			<li> <strong> Datum: </strong> 10.08.16 </li>
+ *			<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 			<li> <strong> Beschreibung: </strong> Impplementierung ActionListener </li>
+ *		</ul>
+ *	</li>
+ *	<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 1.2 </li>
+ *			<li> <strong> Datum: </strong> 12.08.16 </li>
+ *			<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 			<li> <strong> Beschreibung: </strong> Unit-Spezifikation </li>
+ *		</ul>
+ *	</li>
+ * </ol>
+ * 
+ * @version 1.2
  * @author Alexander Reichenbach
  *
  */
