a7cba095cbee0622b30b6050b3c480829e21b344
agesb <hanna.behnke@gmail.com>
2016-08-15 21:51:59 +0200

    Update Änderungshistorie

--- SuchErgebnisPanel.java
+++ SuchErgebnisPanel.java
@@ -7,9 +7,35 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * <strong>Zweck:</strong> Definiert das Design der gelieferten Ergebnisse einer Suche
- * <p><strong>Änderungshistorie:</strong></p>
- * @version 2.1
+ * <strong> Zweck: </strong> Definiert das Design der gelieferten Ergebnisse einer Suche
+ * <h2>Aenderungshistorie:</h2>
+ * <ol>
+ * 	<li>
+ * 		<ul>
+ * 			<li> <strong> Version: </strong> 1.0 </li>
+ *			<li> <strong> Datum: </strong> 06.08.16 </li>
+ *			<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 			<li> <strong> Beschreibung: </strong> Erstellung und Design</li>
+ *		 </ul>
+ *	</li>
+ *	<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 1.1 </li>
+ *			<li> <strong> Datum: </strong> 10.08.16 </li>
+ *			<li> <strong> Autor: </strong> Alexander Reichenbach </li>
+ * 			<li> <strong> Beschreibung: </strong> Implementierung ActionListener und getRenderer() </li>
+ *		</ul>
+ *	</li>
+ *	<li>
+ *		<ul>
+ *			<li> <strong> Version: </strong> 1.2 </li>
+ *			<li> <strong> Datum: </strong> 12.08.16 </li>
+ *			<li> <strong> Autor: </strong> Hanna Behnke </li>
+ * 			<li> <strong> Beschreibung: </strong> Unit-Spezifikation </li>
+ *		</ul>
+ *	</li>
+ * </ol>
+ * @version 1.2
  * @author Alexander Reichenbach
  *
  */
